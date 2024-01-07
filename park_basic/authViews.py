eecf8bbd21c3ed593f626fbb70f670a25a52d056
Aasath <aasadh2000@gmail.com>
2024-01-07 18:07:37 +0530

    token validate

--- park_basic/authViews.py
+++ park_basic/authViews.py
@@ -68,57 +68,94 @@ def RegisterApi(request):
 @parser_classes([JSONParser])
 def LoginApi(request):
     if request.method == 'POST':
-        data = request.data
-        username = data.get('username')
-        password = data.get('password')
 
+        data = request.data
         provided_token = request.META.get('HTTP_AUTHORIZATION')
 
-        if provided_token:
-            try:
-                token_key = provided_token.split(' ')[
-                    1]  # Assuming token is provided in the format "Token <token_value>"
-                token = Token.objects.get(key=token_key)
-                user = token.user
-            except (Token.DoesNotExist, IndexError):
-                user = None
-        else:
+
+        if data:
+
+            username = data.get('username')
+            password = data.get('password')
             user = authenticate(username=username, password=password)
 
-        if user:
+            if user:
 
-            token, created = Token.objects.get_or_create(user=user)
-            if created==False:
-                token.delete()
-                token = Token.objects.create(user=user)
-            Reserver = reserver.objects.filter(userId=user.id).first()
+                token, created = Token.objects.get_or_create(user=user)
+                if created==False:
+                    token.delete()
+                    token = Token.objects.create(user=user)
+                Reserver = reserver.objects.filter(userId=user.id).first()
 
 
-            user_info = {
-                "is_superuser": user.is_superuser,
-                "first_name": user.first_name,
-                "last_name": user.last_name,
-                "email": user.email
-            }
-            if Reserver:
-                reserver_info = {
-                    "carNo": Reserver.carNo,
-                    "phnNo": Reserver.phnNo
+                user_info = {
+                    "is_superuser": user.is_superuser,
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "email": user.email
                 }
-                return JsonResponse({
-                    "user":user_info,
-                    "reserver": reserver_info,
-                    "token": token.key,
-                    "message": True,
-                }, status=status.HTTP_200_OK)
+                if Reserver:
+                    reserver_info = {
+                        "carNo": Reserver.carNo,
+                        "phnNo": Reserver.phnNo
+                    }
+                    return JsonResponse({
+                        "user":user_info,
+                        "reserver": reserver_info,
+                        "token": token.key,
+                        "message": True,
+                    }, status=status.HTTP_200_OK)
+                else:
+                    return JsonResponse({
+                        "user": user_info,
+                        "token": token.key,
+                        "message": True,
+                    }, status=status.HTTP_200_OK)
+
             else:
                 return JsonResponse({
-                    "user": user_info,
-                    "token": token.key,
-                    "message": True,
-                }, status=status.HTTP_200_OK)
+                    'error': 'Invalid credentials or token',
+                }, status=status.HTTP_401_UNAUTHORIZED)
 
         else:
-            return JsonResponse({
-                'error': 'Invalid credentials or token',
-            }, status=status.HTTP_401_UNAUTHORIZED)
+            if provided_token and provided_token.startswith('Bearer '):
+                token_key = provided_token.split(' ')[1]
+
+                try:
+                    token = Token.objects.get(key=token_key)
+                    user = token.user
+                    Reserver = reserver.objects.filter(userId=user.id).first()
+
+                    user_info = {
+                        "is_superuser": user.is_superuser,
+                        "first_name": user.first_name,
+                        "last_name": user.last_name,
+                        "email": user.email
+                    }
+
+                    if Reserver:
+                        reserver_info = {
+                            "carNo": Reserver.carNo,
+                            "phnNo": Reserver.phnNo
+                        }
+                        return JsonResponse({
+                            "user": user_info,
+                            "reserver": reserver_info,
+                            "token": token.key,
+                            "message": True,
+                        }, status=status.HTTP_200_OK)
+                    else:
+                        return JsonResponse({
+                            "user": user_info,
+                            "token": token.key,
+                            "message": True,
+                        }, status=status.HTTP_200_OK)
+
+                except Token.DoesNotExist:
+                    return JsonResponse({
+                        "message": "Invalid token"
+                    }, status=status.HTTP_401_UNAUTHORIZED)
+
+                return JsonResponse({
+                    "message": "Token not provided or invalid format"
+                }, status=status.HTTP_401_UNAUTHORIZED)
