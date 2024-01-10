2fcca4baae221e760686c0f2aedc536b0a5aad1a
Aasath <aasadh2000@gmail.com>
2024-01-10 22:32:01 +0530

    booking all info

--- park_basic/mathuView.py
+++ park_basic/mathuView.py
@@ -15,17 +15,29 @@ from django.contrib.auth import authenticate
 @parser_classes([JSONParser])
 def GetAllBookingAPI(request):
     bookings = booking.objects.all()
+    data = []
+
     for booking_instance in bookings:
         reserver_instance = booking_instance.reserverId
         if reserver_instance:
-            reserver_data = {
-                'carNo': reserver_instance.carNo,
-                'phnNo': reserver_instance.phnNo,
-                # Add other fields as needed
-            }
-            # print(reserver_instance.carNo)
-            booking_instance.reserverId = JsonResponse({reserver_data})
-    bookings_serializer=BookingSerializer(bookings,many=True)
+            user_instance = reserver_instance.userId
+
+            if user_instance:
+                user_data = {
+                    'first_name': user_instance.first_name,
+                    'last_name': user_instance.last_name,
+                    'email': user_instance.email,
+                }
+                reserver_data = ReserverSerializer(reserver_instance).data
+                booking_data = BookingSerializer(booking_instance).data
+                all_data = {
+                'booking_data': booking_data,
+                'reserver_data' : reserver_data,
+                'user_data' : user_data,
+
+                }
+                data.append(all_data)
+
     return JsonResponse({
-        'bookings': bookings_serializer.data
+        'bookings': data
     }, safe=False, status=200)
